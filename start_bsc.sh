5bcb2598fde87d5fd604405e03a69c535c4638ed
seerday <nixos@vmi528573.contaboserver.net>
2021-04-16 11:07:10 +0200

    rm rewards ready for tests on kovan

--- start_bsc.sh
+++ start_bsc.sh
@@ -5,14 +5,14 @@ export LIBEXEC_DIR='libexec'
 export CONFIG_DIR='config'
 export DAPP_LIB='contracts'
 export DAPP_SKIP_BUILD='yes'
-export ETH_FROM=0xF46bb3243453a36Edd84EE7b19E801c0437bE773
-export ETH_GAS_PRICE=1000000000
+export ETH_FROM=0x701123a676E9A765191276C1587c01b009646EF6
+export ETH_GAS_PRICE=10000000000
 export ETH_PASSWORD=password
 export ETH_KEYSTORE=keys/keystore
-export SETH_CHAIN=bsc
+export SETH_CHAIN=bsctest
 export ETH_RPC_URL=https://data-seed-prebsc-1-s3.binance.org:8545
 export ETHERSCAN_API_KEY=DUJ2SA4AXYMAIUM4AQCD74I85FP7CXFA3R
 
 # seth ls
-bin/dss-deploy bsc
+bin/dss-deploy bsctest
 
